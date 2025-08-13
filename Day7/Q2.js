fbfe17d609574f657d48de9faddadb17aedebd34
harishkannan4848 <harikrisnan2002@gmail.com>
2025-08-13 20:01:24 +0530

    Day 7

---
+++ Day7/Q2.js
@@ -0,0 +1,34 @@
+/** Q2
+	 * Write a Java program to calculate Simple Interest using the formula:
+ 		SI = (P × N × R) / 100
+ 		Where:
+		P = Principal amount
+		N = Number of years
+		R = Rate of interest
+		
+		Problem Flow:
+		User inputs:
+			Principal (P)
+			Rate (R)
+			Years (N)
+		Program calculates:
+			SI = (P × N × R) / 100
+		Output displays the Simple Interest value.
+		
+		Input / Output Example:
+		Input:
+		Enter Principal Amount: 10000  
+		Enter Rate of Interest: 5  
+		Enter No. of Years: 2
+		Output:
+		Simple Interest = 1000.00
+*/
+
+
+let p=prompt("enter the principal amount");
+let n=prompt("enter the number of years");
+let r=prompt("enter the rate of interest");
+
+   SI=(p*n*r)/100
+   console.log(SI);
+   
